8cf93e91f57aa64febf41ca51a153eefff00ee42
vikas937042 <96293449+vikas937042@users.noreply.github.com>
2022-05-12 22:40:57 +0530

    Update que2.cpp

--- Assignment 2/setA/que2.cpp
+++ Assignment 2/setA/que2.cpp
@@ -1,55 +1,32 @@
-/*
-
-Create a class named 'DISTANCE' with feet and inches as data members. 
-The class has the following member function
-a. To input distance
-b. To output distance
-c. To add two distance objects
-Write a C++ program to create objects of DISTANCE class. Input two distance and output 
-the sum.
-*/
-#include<iostream>
+#include<iostream.h>
 #include<conio.h>
-using namespace std;
 class DISTANCE
 {
     private : 
-    int feet ,inches;
+    	int feet ,inches;
     public  : 
-    void setData(void);
-    int addobj(DISTANCE obj1);
-    void output(void);
+	void setData(void)
+	{
+	    cout<<"Enter feet and inches"<<endl;
+	    cin>>feet>>inches;
+	}
+	int addobj(DISTANCE obj1)
+	{
+		DISTANCE temp;
+		temp.feet = feet.obj1.feet;
+		temp.inches = inches.obj1.inches;
+		return temp;
+	}
+	void output(void)
+	{
+	    cout<<"Total is : "<<feet<<" feet and "<<inches<<" inches";
+	}
 };
-void DISTANCE :: setData(void)
-{
-    cout<<"Enter feet and inches"<<endl;
-    cin>>feet>>inches;
-
-}
-int DISTANCE :: addobj(DISTANCE obj1)
-{
-	DISTANCE temp;
-	temp.feet = feet.obj1.feet;
-	temp.inches = inches.obj1.inches;
-	
-    
-    	return temp;
-	
-
-}
-void DISTANCE :: output(void)
-{
-    cout<<"The distance of two object is : "<<feet<<" feet and "<<inches<<" inches";
-
-}
-int main()
+void main()
 {
     DISTANCE obj1,obj2,obj3;
-    cout<<"For Object 1"<<endl;
     obj1.setData();
-    cout<<"For Object 2"<<endl;
     obj2.setData();
-	obj3 = obj1.add(obj(obj2));
-	obj3.output();
-    return 0;
+    obj3 = obj1.add(obj(obj2));
+    obj3.output();
 }
