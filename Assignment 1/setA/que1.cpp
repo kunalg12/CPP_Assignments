33f8fa1dc8b06ff381bb87314dbc02c1f7d6fdb1
vikas937042 <96293449+vikas937042@users.noreply.github.com>
2022-05-12 21:23:11 +0530

    Update que1.cpp

--- Assignment 1/setA/que1.cpp
+++ Assignment 1/setA/que1.cpp
@@ -1,31 +1,18 @@
-/*
-    Write a C++ program to print Floyd's triangle.
-
-    1
-    2 3
-    4 5 6
-    7 8 9 10
-
-*/
-
-#include <iostream>
+#include <iostream.h>
 #include <conio.h>
-using namespace std;
-int main()
+void main()
 {
     
-    int num, temp = 1, i,j ;
+    int len, i,j ,temp = 1;
     cout<<"Enter number"<<endl;
-    cin>>num;
-    for(i = 1; i <= num; i++)
+    cin>>len;
+    for(i = 1; i <= len; i++)
     {
         for ( j = 1; j<= i; ++j)
         {
             cout<<temp<<" ";
-            ++temp;
+            temp++;
         }
         cout<<endl;
     }
-    return 0;
 }
-//0.56.61
