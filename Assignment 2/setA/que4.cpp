bc2ab29b9798a4c05ebb913e16dd03de393710aa
vikas937042 <96293449+vikas937042@users.noreply.github.com>
2022-05-12 22:57:34 +0530

    Update que4.cpp

--- Assignment 2/setA/que4.cpp
+++ Assignment 2/setA/que4.cpp
@@ -1,115 +1,100 @@
-/*
-    Define a class string to perform different operations.
-    a.To find length of string
-    b.To concatenate two strings.
-    c.To reverse the string
-    d.To compare two strings
-
-*/
-#include <iostream>
-
-#include<iomanip>
+#include <iostream.h>
 #include <string.h>
 #include <conio.h>
-using namespace std;
 class STRING
 {
-private:
-    char str1[50], str2[50];
+    private:
+        char str1[10], str2[10];
 
-public:
-    void setstr(void);
-    void len(void);
-    void concat(void);
-    void rev(void);
-    void cmp(void);
+    public:
+        void setstr()
+        {
+            cout << "Enter first string : ";
+            getchar();
+            cin>>str1;
+            cout << endl;
+            cout << "Enter second string: ";
+            cin>>str2;
+        }
+        void len()
+        {
+            cout << "The Length of the First string is : " << strlen(str1) << endl;
+            cout << "The Length of the Second string is : " << strlen(str2) << endl;
+        }
+        void concat()
+        {
+            cout << "Concatenated string is : " << strcat(strcat(str1, " "), strcat(str2, " ")) << endl;
+        }
+
+        void rev()
+        {
+            cout << "Enter first string : ";
+            getchar();
+            gets(str1);
+            cout << endl;
+            cout << "Your  string is : " << str1 << endl;
+            cout << "Your Reverse String is : " << strrev(str1) << endl;
+
+        }
+        void cmp()
+        {
+            int i, count = 0;
+            if(strlen(str1)!=strlen(str2)){
+                cout<<"String is not same"<<endl;
+            }
+            else{
+                for (i = 0; i <= strlen(str1); i++)
+                    {
+                        if (str1[i] == str2[i])
+                        {
+                            count++;
+                            cout << str1[i];
+                        }
+                    }
+                    if (count <= 0)
+                    {
+                        cout << "String is not same" << endl;
+                        cout << count;
+                    }
+                    else
+                    {
+                        cout << "String is same" << endl;
+                        cout << count;
+                    }
+            }
+        }
 };
 
-void STRING ::setstr(void)
-{
-    cout << "Enter first string : ";
-    getchar();
-    gets(str1);
-    cout << endl;
-    cout << "Enter second string: ";
-    gets(str2);
-    cout << endl;
-}
-void STRING ::len(void)
-{
-    cout << "The length of the first string is : " << strlen(str1) << endl;
-    cout << "The length of the second string is : " << strlen(str2) << endl;
-}
-void STRING ::concat(void)
-{
-    cout << "The first string is : " << str1 << endl;
-    cout << "The second string is : " << str2 << endl;
-    cout << "Concat of two string is : " << strcat(strcat(str1, " "), strcat(str2, " ")) << endl;
-}
 
-void STRING ::rev(void)
-{
-    cout << "Enter first string : ";
-    getchar();
-    gets(str1);
-    cout << endl;
-    cout << "Your  string is : " << str1 << endl;
-    cout << "Your Reverse String is : " << strrev(str1) << endl;
-  
-}
-void STRING ::cmp(void)
-{
-    int i, count = 0;
-
-    for (i = 0; i <= strlen(str1); i++)
-    {
-        if (str1[i] == str2[i])
-        {
-            count++;
-            cout << str1[i];
-        }
-    }
-    if (count <= 0)
-    {
-        cout << "String is not same" << endl;
-        cout << count;
-    }
-    else
-    {
-        cout << "String is same" << endl;
-        cout << count;
-    }
-}
-int main()
+void main()
 {
     STRING S1;
-    int opt;
-    cout << setw(20) << setfill(' ') << "Enter 1. Find length of strings" << endl;
-    cout << setw(20) << setfill(' ') << "Enter 2. Concat two string" << endl;
-    cout << setw(20) << setfill(' ') << "Enter 3. Reverse string" << endl;
-    cout << setw(20) << setfill(' ') << "Enter 4. Compare two strings" << endl;
-    cin>>opt;
-    switch(opt)
+    int option;
+    S1.setstr();
+    cout << "Enter 1. Find length of strings" << endl;
+    cout << "Enter 2. Concat two string" << endl;
+    cout << "Enter 3. Reverse string" << endl;
+    cout << "Enter 4. Compare two strings" << endl;
+    cin>>opt;    
+    switch(option)
     {
         case 1: 
-          S1.setstr(); S1.len();
+          S1.len();
           break;
         case 2: 
-             S1.setstr(); S1.concat();
+            S1.concat();
             break;
         case 3: 
-              S1.rev();
+             S1.rev();
             break;
         case 4:
-             S1.setstr(); S1.cmp();
+             S1.cmp();
              break;
         case 5: 
             exit(0);
             break;
         default : 
             cout<<"Invalid Input";
-            break;
-   
+            break;   
     }
-    return 0;
 }
