6c6a1ddae8cb285238c71790ef68e7aabc4b3da3
vikas937042 <96293449+vikas937042@users.noreply.github.com>
2022-05-12 21:26:27 +0530

    Update que2.cpp

--- Assignment 1/setA/que2.cpp
+++ Assignment 1/setA/que2.cpp
@@ -1,28 +1,18 @@
-/*2. Write a C++ program to print the pattern
-A 
-B C 
-D E F 
-G H I J
-*/
-#include<iostream>
+#include<iostream.h>
 #include<conio.h>
-using namespace std;
-int main()
+void main()
 {
-    int num;
+    int len,i,j;
     char temp = 'A';
-    int i,j;
     cout<<"Enter number";
-    cin>>num;
-    for(i = 1;i<=num;i++)
+    cin>>len;
+    for(i=1;i<=len;i++)
     {
-        for(j = 1;j<=i;++j)
+        for(j=1;j<=i;j++)
         {
             cout<<temp<<" ";
-            ++temp;
+            temp++;
         }
         cout<<endl;
     }
-    return 0;
 }
-// 1.38.33
